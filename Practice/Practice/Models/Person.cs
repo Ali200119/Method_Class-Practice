29e307fcdbbcdbedc4f7cbe642db988109f724ef
Ali200119 <aliit@code.edu.az>
2022-12-20 05:58:12 +0400

    feat: Task 1

---
+++ Practice/Practice/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Practice.Models
+{
+	public class Person
+	{
+		public string Name { get; set; }
+		public string Surname { get; set; }
+		public int Age { get; set; }
+		public bool IsMarried { get; set; }
+
+		public Person(string name, string surname, int age, bool isMaried)
+		{
+			Name = name;
+			Surname = surname;
+			Age = age;
+			IsMarried = isMaried;
+		}
+	}
+}
+
---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,44 @@
+﻿using Practice.Models;
+
+#region Task 1
+
+//int age = 22;
+
+//int result = CountOfMarried(age);
+
+//Console.WriteLine(result);
+
+//static Person[] GetPersons()
+//{
+//    Person person1 = new Person("Ali", "Talibov", 21, true);
+//    Person person2 = new Person("Cavid", "Bashirov", 29, false);
+//    Person person3 = new Person("Rustam", "Hacizade", 25, true);
+//    Person person4 = new Person("Teymur", "Isayev", 23, false);
+
+//    return new Person[] { person1, person2, person3, person4 };
+//}
+
+//static int CountOfMarried(int age)
+//{
+//    Person[] persons = GetPersons();
+
+//    int count = 0;
+
+//    foreach (Person person in persons)
+//    {
+//        if (person.Age > age && person.IsMarried)
+//        {
+//            count++;
+//        }
+//    }
+
+//    return count;
+//}
+
+#endregion
+
+#region Task2
+
+
+
+#endregion
